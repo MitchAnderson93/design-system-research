770d0f49dff1a9f8cb15199fd35935484e6e8df9
Mitch Anderson <mitchell.anderson@smartservice.qld.gov.au>
2023-03-12 16:48:56 +1000

    KR7 demo fixups

--- design-system/src/index.js
+++ design-system/src/index.js
@@ -3,4 +3,4 @@ import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js';
 import '@webcomponents/webcomponentsjs/webcomponents-bundle.js';
 
 //Components
-import './components/button/button';
+import './components/button';
